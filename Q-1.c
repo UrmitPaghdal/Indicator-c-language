ee774135b2f3ff30c2ac42be568cfc12a357738c
Abhaykahar <ak2415112@gmail.com>
2024-02-28 14:53:17 +0530

    first commit

---
+++ Q-1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main(){
+	
+	int a=0;  
+	char *p;
+	char u[100]; 
+
+	printf("Enter any string : ");
+	gets(u);
+
+	p = a;
+
+	a = strlen(u);
+
+	printf("lenth is : %d\n",a);
+
+}
+
